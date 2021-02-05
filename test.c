3211272ac899ea05f9853a77740481832ceaab3d
young <yzcsmail@gmail.com>
2021-02-05 20:43:12 +0800

    Learning C

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include"t.hello"
-
-void main(){
-	printf("%d",f(2));
+void main()
+{	
+	struct date day;
+	day.day = 1;
+	day.month = 2;
+	day.year = 2012;
+	printf("%i/%.2i/%.2i",day.year,day.month,day.day);
+	printf("%d\n",f(2));
 }
